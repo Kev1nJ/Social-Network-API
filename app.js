bf30f1d43bdbc77c340976e75890b590f418c12f
Kev1nJ <kevinjerome85@yahoo.com>
2023-11-07 19:35:18 -0500

    main app.js pahts created, modules improted ,  baseline structure for routes and controllers created

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 // app.js
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const db = require('./config/db'); 
 
+require('dotenv').config();
 
+// Connect to the MongoDB database
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Define your API routes
+const usersRoutes = require('./routes/users'); 
+const thoughtsRoutes = require('./routes/thoughts'); 
+
+app.use('/users', usersRoutes); 
+app.use('/thoughts', thoughtsRoutes);
+
+// Error handling middleware (not shown in this example, but you should include it)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require('../models/User');
+
+const UserController = {
+  register: async (req, res) => {
+    // Logic to register a new user
+  },
+  login: async (req, res) => {
+    // Logic to authenticate and log in a user
+  },
+  // Implement more controller functions as needed
+};
+
+module.exports = UserController;
